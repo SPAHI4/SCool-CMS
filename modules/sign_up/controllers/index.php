b26b5a13d0ef8cb11afc581d263e4df2b130e205
SPAHI4 <spaxi4@yandex.ru>
2012-05-06 16:52:59 +0400

    Update modules/sign_up/controllers/index.php

--- modules/sign_up/controllers/index.php
+++ modules/sign_up/controllers/index.php
@@ -4,7 +4,7 @@ Class Controller_Index Extends Controller {
 	
 	function index() {
 	$this->title="Регистрация";
-	$this->only_for="unauth";
+    $this->only_for="anauth";
 		if(isset($_POST['sendData'])){
 			if(!Captcha::check()){
 				$this->addError('Неверное проверочное число');
@@ -13,7 +13,7 @@ Class Controller_Index Extends Controller {
 			if(strlen(trim($_POST['login']))<3)$this->addError('Короткий логин');
 			else{
 				if(!preg_match("#^([A-zА-я0-9\-\_\ ])+$#ui", $_POST['login']))$this->addError('В логине присутствуют запрещенные символы');
-				if (preg_match("#[a-z]+#ui", $_POST['login']) && preg_match("#[а-я]+#ui", $_POST['login']))$this->addError('Разрешается использовать символы только русского или только английского алфавита');
+				if (preg_match("#[A-z]+#ui", $_POST['login']) && preg_match("#[А-я]+#ui", $_POST['login']))$this->addError('Разрешается использовать символы только русского или только английского алфавита');
 				if($this->db->query("select count(*) from `users` where `login` = '".addslashes($_POST['login'])."';")->fetchColumn())$this->addError('Данный логин уже занят');
 				if(trim(strlen($_POST['login']))>16)$this->addError('Длинный логин');
 				if(trim(strlen($_POST['login']))<3)$this->addError('Длинный логин');
@@ -24,12 +24,13 @@ Class Controller_Index Extends Controller {
 			if(strlen(trim($_POST['password']))<6)$this->addError('Короткий пароль');
 			
 			if(!$this->errors){
-				$this->db->query("insert into `users` (`login`,`password`,`reg_timestamp`,`ip`,`sex`) values ('".inputFilter(trim($_POST['login']))."','".hashEncode(trim($_POST['password']))."',UNIX_TIMESTAMP(NOW()),'".inputFilter(clientIP())."',".(bool)$_POST['sex'].");");
+				$this->db->query("insert into `users` (`login`,`password`,`reg_timestamp`,`sex`) values ('".addslashes(trim($_POST['login']))."','".hashEncode(trim($_POST['password']))."',UNIX_TIMESTAMP(NOW()),'".(bool)$_POST['sex']."')");
 				$uid=$this->db->lastInsertId();
-				setCookie('uid',$uid,time()+3500000);
-				setCookie('hash',hashEncode($_POST['password']),time()+3500000);
+				setCookie('uid',$uid,time()+3500000,"/");
+				setCookie('hash',hashEncode($_POST['password']),time()+3500000,"/");
 				$_SESSION['user_id']=$uid;
-				header("location: /my/?welcome");
+				header("Location: /profile/");
+				
 			}
 			
 		}
